8d8dc35dc14427ba2a5099c14f4f48fa03010182
RF216005 <F216005@ed.tusy.ac.jp>
2018-09-30 22:53:26 +0900

    課題完了

---
+++ app/src/main/java/com/example/enpit_p3/chapter11_myalartclock/Dialogs.kt
@@ -0,0 +1,101 @@
+package com.example.enpit_p3.chapter11_myalartclock
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.widget.DatePicker
+import android.widget.TimePicker
+import java.util.*
+
+
+class SimpleAlartDialog : DialogFragment() {
+
+    interface OnClickListener {
+        fun onPositiveClick()
+        fun onNegativeClick()
+    }
+
+    private lateinit var listener: OnClickListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is SimpleAlartDialog.OnClickListener) {
+            listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val builder = AlertDialog.Builder(activity).apply {
+            setMessage("時間になりました！")
+            setPositiveButton("起きる") {dialog, which ->
+                listener.onPositiveClick()
+            }
+            setNegativeButton("あと5分") {dialog, which ->
+                listener.onNegativeClick()
+            }
+        }
+        return builder.create()
+    }
+}
+
+class DatePickerFragment : DialogFragment(),
+        DatePickerDialog.OnDateSetListener {
+
+    interface OnDateSelectedListener {
+        fun onSelected(year: Int, month: Int, date: Int)
+    }
+
+    private lateinit var listener: OnDateSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnDateSelectedListener) {
+            listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c =Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(context, this, year, month, date)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, date: Int) {
+        listener.onSelected(year, month, date)
+    }
+}
+
+class TimePickerFragment : DialogFragment(),
+        TimePickerDialog.OnTimeSetListener {
+
+    interface OnTimeSelectedListener {
+        fun onSelected(hourDialog: Int, minute: Int)
+    }
+
+    private lateinit var listener : OnTimeSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is TimePickerFragment.OnTimeSelectedListener) {
+            listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+        return TimePickerDialog(context, this, hour, minute, true)
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        listener.onSelected(hourOfDay, minute)
+    }
+}
